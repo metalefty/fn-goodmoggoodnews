2aeb0d610f5c4731789906aa9a88d1ada283e295
Koichiro Iwao <meta@vmeta.jp>
2021-07-10 13:05:07 +0000

    Implement Goodmoggoodnews

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://www.rubygems.org' do
   gem 'fdk', '>= 0.0.24'
+  gem 'goodmoggoodnews', '~> 0.3.0', source: 'http://icepick.vmeta.jp:8080'
 end
--- func.rb
+++ func.rb
@@ -1,10 +1,20 @@
 require 'fdk'
+require 'goodmoggoodnews'
 
-def myfunction(context:, input:)
+def goodmogrun(context:, input:)
   input_value = input.respond_to?(:fetch) ? input.fetch('name') : input
   name = input_value.to_s.strip.empty? ? 'World' : input_value
   FDK.log(entry: "Inside Ruby Hello World function")
-  { message: "Hello #{name}!" }
+  
+  twitter = Goodmoggoodnews::Twitter.new
+  before_location = twitter.location
+
+  Goodmoggoodnews::Runner.go
+  
+  after_location = twitter.location
+  location = after_location.merge(u: !(before_location === after_location))
+
+  location
 end
 
-FDK.handle(target: :myfunction)
+FDK.handle(target: :goodmogrun)
